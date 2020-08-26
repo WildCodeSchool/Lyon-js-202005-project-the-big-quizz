b917c6551d7fa1c8d4916f890edf4b2aa22fe410
shivan paishari <shivanpaishari@gmail.com>
2020-08-26 15:21:51 +0200

    ajout du select

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import NbPlayer from "./components/NbPlayers";
+import GameParameters from './components/GameParameters';
 
 function App() {
   return (
     <div className="App">
      
      <NbPlayer />
-
+    <GameParameters/>
     </div>
   );
 }
