5cd21c0ee1036ea70e3ffbb3b9075bdd1c83d39c
STAM0283 <aminestambouli00780@gemail.com>
2020-08-26 11:27:29 +0200

    question of the day

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import NbPlayer from "./components/NbPlayers";
+import QuestionOfTheDay from "./components/QuestionOfTheDay"; 
 
 function App() {
   return (
     <div className="App">
      
      <NbPlayer />
+     <QuestionOfTheDay />
 
     </div>
   );
