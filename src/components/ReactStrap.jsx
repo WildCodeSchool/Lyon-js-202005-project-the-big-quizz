a074ff87ec74d02bc81d96e4df2ea994a398803a
Ismael <ismael.bernard@epitaphstudios.fr>
2020-08-27 15:39:19 +0200

    Added Documentation about reactstrap

--- src/components/ReactStrap.jsx
+++ src/components/ReactStrap.jsx
@@ -6,6 +6,21 @@ function ReactStrap () {
     return(
         <div>
         <h2> Welcome to the ReactStrap Sandbox tutorial</h2>
+        <a href="https://reactstrap.github.io/">Lien de la Doc de Reactstrap</a>
+        <br></br>
+        <h6> Avant de commencer, il y a quelques commandes npm à faire :</h6>
+        <br></br>
+        <p>npm install --save reactstrap</p>
+        <p>npm install --save bootstrap</p>
+        <br></br>
+        <h6>Deuxième étape : importer la commande suivante dans son fichier index.js</h6>
+        <p>import 'bootstrap/dist/css/bootstrap.min.css';</p>
+        <br></br>
+
+
+        <span>
+           
+        </span>
         </div>
     );
 }
