5348507c33c04df00919888a8a533e8d5cc9581c
Ismael <ismael.bernard@epitaphstudios.fr>
2020-08-27 14:43:25 +0200

    Added a ReactStrap component as a page test

---
+++ src/components/ReactStrap.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+Function ReactStrap () {
+    return(
+        <div>
+        <h2> Welcome to the ReactStrap Sandbox tutorial</h2>
+        </div>
+    );
+}
+
+
+
+
+export default ReactStrap; 
