2be7f2339ff111a13a84584aaf67dc0485aae4f9
Madalina <madalina.zamfira@gmail.com>
2020-08-27 16:00:44 +0200

    added progress bar

--- src/components/ReactStrap.jsx
+++ src/components/ReactStrap.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { Progress } from 'reactstrap';
 
 
 function ReactStrap () {
@@ -13,4 +14,17 @@ function ReactStrap () {
 
 
 
-export default ReactStrap; 
+const ProgressExample = (props) => {
+  return (
+    <div>
+      <h2>Progress bar example</h2>
+      <Progress value={75}>You're almost there!</Progress>
+      <br />
+      <Progress color="success" value="100">You did it!</Progress>
+    </div>
+  );
+};
+export default ProgressExample;
+
+
+//export default ReactStrap; 
