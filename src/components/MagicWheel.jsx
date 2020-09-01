f0a3062025311739a0962e91075e2a8f6574324e
Ismael <ismael.bernard@epitaphstudios.fr>
2020-09-01 20:17:07 +0200

    Added class to react

--- src/components/MagicWheel.jsx
+++ src/components/MagicWheel.jsx
@@ -4,9 +4,19 @@ import Wheel from "../images/wheel.png";
 import Selector from "../images/selector.png";
 import WheelRotation from "./WheelRotation";
 import "../App.css";
+import { useState } from "react";
 
-function MagicWheel() {
-  return (
+class MagicWheel extends React.Component {
+  state = {
+    id: "WheelContainer"
+  }
+    squizTheWheel = () => {
+      this.setState({
+        name:"WheelContainer start-rotate"
+      });
+    }
+  render (){
+    return (
     <>
       <h1>This is a Magic Wheel !</h1>
       <div className="WheelContainer">
@@ -14,23 +24,27 @@ function MagicWheel() {
           className="Selector"
           src={Selector}
           alt="wheel selector"
-          height={600}
+          height={500}
           width={600}
         ></img>
         <img
           className="Wheel"
+          id ="Wheel"
           src={Wheel}
           alt="wheel of questions"
           heigth={500}
           width={500}
         ></img>
+        <br></br>
+        <br></br>
       </div>
       <div>
-        <button className="MagicButton">Spin The Wheel !</button>
+        <button className="magicButton">🎰 Squiz-Me ! 🎰</button>
         <script src={WheelRotation}></script>
       </div>
     </>
   );
+    }
 }
 
 export default MagicWheel;
