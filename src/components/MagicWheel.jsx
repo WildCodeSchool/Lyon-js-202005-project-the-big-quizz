5a689ec0fe98be23866cb6a89b81551bc07a2393
Niters <niters69@gmail.com>
2020-09-01 11:05:11 +0200

    debuuged selector.png

--- src/components/MagicWheel.jsx
+++ src/components/MagicWheel.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Wheel from "../images/wheel.png";
+import Selector from "../images/selector.png";
+import "../App.css";
 
 function MagicWheel() {
   return (
-    <div>
+    <>
       <h1>This is a Magic Wheel !</h1>
-    </div>
+      <div className="WheelContainer">
+        <img
+          className="Selector"
+          src={Selector}
+          alt="wheel selector"
+          height={50}
+          width={60}
+        ></img>
+        <img
+          className="Wheel"
+          src={Wheel}
+          alt="wheel of questions"
+          heigth={500}
+          width={500}
+        ></img>
+      </div>
+    </>
   );
 }
 
